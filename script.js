d6a36adeee6db41086c8c70c651f02de14b91b3b
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-26 13:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function mapLetter(s) {
+	let map = {};
+
+	for(let i in s){
+		if(!map[s.charAt(i)]) {
+			map[s.charAt(i)] = [i];
+		}else{
+			map[s.charSt(i)].push(i);
+		}
+	}
+
+	return map;
+}
+
+let input = prompt("Enter the string");
+alert(mapLetter(input)); 
